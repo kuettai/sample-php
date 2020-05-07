78eb5f98de47b903e97c7e80d861619eb5692aac
KuetTai <kt.xtrik@gmail.com>
2020-05-07 22:57:09 +0800

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-S3BUCKET='kuettai-asglogdemo'
+S3BUCKET='	kuettai-asglogdemo-test'
 BACKUPDIRECTORY='/var/log'
 ASGNAME='asgDemo'
 HOOKRESULT='CONTINUE'
@@ -7,6 +7,6 @@ LIFECYCLEHOOKNAME='asgHookTERM'
 INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=${REGION::-1}
-tar -cf /tmp/${INSTANCEID}.tar $BACKUPDIRECTORY &> /tmp/backup
-aws s3 cp /tmp/${INSTANCEID}.tar s3://${S3BUCKET}/${INSTANCEID}/ &> /tmp/backup
+tar -czf /tmp/${INSTANCEID}.tar.gz $BACKUPDIRECTORY &> /tmp/backup
+aws s3 cp /tmp/${INSTANCEID}.tar.gz s3://${S3BUCKET}/${INSTANCEID}/ &> /tmp/backup
 aws autoscaling complete-lifecycle-action --lifecycle-hook-name ${LIFECYCLEHOOKNAME} --auto-scaling-group-name ${ASGNAME} --lifecycle-action-result ${HOOKRESULT} --instance-id ${INSTANCEID}  --region ${REGION}
